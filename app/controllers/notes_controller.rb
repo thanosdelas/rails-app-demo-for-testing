33fa931230c2bb725227cb68b38488a15e3143df
delasthanos <delasthanos@gmail.com>
2021-05-16 16:13:20 +0300

    Add not setup and configuration notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,10 @@
+class NotesController < ApplicationController
+
+  def initialize
+    super
+    @page = "notes"
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
   root "index#index"
 
+  get "notes", to: "notes#index", as: :notes
+
   # get "products", to: "products#index"
   # get "products/listing", to: "products#listing"
   # get "products/:id", to: "products#show"
